023379982add96a4d89647f2c32b03c508be06d8
Andrea Ferlini <AndreaFerlini@MacBook-Pro-di-Ferla.local>
2015-11-19 14:38:10 +0100

    suddivisione in View-Model-Controller

--- catapp.js
+++ catapp.js
@@ -1,87 +1,88 @@
-var clicknumb=0;
-
 var temp = "<li id='ID'><h1>NAME</h1><img src='IMG' width='200' height='200'><div id='count_X'> # clicks: CLICK_ON</div></li> ";
-
 var temp_name = "<li id='ID'> <h1>NAME</h1> </li> ";
 
 $(document).ready(function(){
-  hide_admin();
-  load_name();
+  view.hide_admin();
+  view.load_name();
 });
 
-/* FUNCTIONS */
-
-function hide_admin(){
-  $(".btn-admin").hide();
-  $(".admin-mod").hide();
-};
-
-function load_name(){
-  $(".cats_names").html("");
-  $(".cats").html("");
-  $(".cats_names").show();
-  for(var i=0; i<data.length; i++){
-    var temp2;
-    temp2 = temp_name.replace("ID", i).replace("NAME", data[i].cat_name);
-    $(".cats_names").append(temp2);
-  }
-  click_on_cat();
-};
-
-function click_on_cat(){
-  $(".cats_names li").click(function(){
-    $(".cats_names").hide();
-    load_sel_cat($(this).attr('id'));
-  })
-};
-
-function load_sel_cat(i){
-  var temp1;
-  temp1 = temp.replace("ID", i).replace("NAME", data[i].cat_name)
-          .replace("IMG", data[i].img).replace("X", i).replace("CLICK_ON", data[i].click_number);
-  $(".cats").append(temp1);
-  click_counter();
-  $(".btn-admin").show();
-  click_admin();
-};
-
-function click_counter(){
-  $(".cats li").click(function(){
-
-    var k = $(this).attr('id');
-    data[k].click_number ++;
-    $("#count_"+k).html("# clicks: "+data[k].click_number);
-  })
-};
-
-/*ADMIN MODE FUNCTIONS*/
-function click_admin(){
-  $(".btn-admin").click(function(){
-    var n=$(".cats li").attr('id');
-    $(".admin-mod").show();
-    $("input[name=cat-name-text]").val(data[n].cat_name);
-    $("input[name=cat-clicks-text]").val(data[n].click_number);
-    click_cancel();
-    click_save();
-
-  })
-};
-
-function click_cancel(){
+/* VIEW, tutta la modifica del DOM, non interagisce MAI con MODEL */
+var view = {
+  load_name : function (){
+    $(".cats_names").html("");
+    $(".cats").html("");
+    $(".cats_names").show();
+    controller.loading_names();
+    view.click_on_cat();
+  },
+  click_on_cat : function (){
+    $(".cats_names li").click(function(){
+      $(".cats_names").hide();
+      controller.load_sel_cat($(this).attr('id'));
+    });
+  },
+  click_admin : function (){
+    $(".btn-admin").click(function(){
+      var n=$(".cats li").attr('id');
+      $(".admin-mod").show();
+      controller.init_admin_textbox(n);
+      view.click_cancel();
+      view.click_save();
+    })
+  },
+  click_cancel : function (){
     $(".btn-cancel").click(function(){
-        hide_admin();
-        load_name();
+        view.hide_admin();
+        view.load_name();
         //location.refresh(); //è il refresh della pagina!
     })
+  },
+  click_save : function (){
+    $(".btn-save").click(function(){
+      controller.save();
+      alert ("New content saved!");
+      view.hide_admin();
+      view.load_name();
+    })
+  },
+  hide_admin : function (){
+    $(".btn-admin").hide();
+    $(".admin-mod").hide();
+  },
 };
 
-function click_save(){
-    $(".btn-save").click(function(){
-      var n=$(".cats li").attr('id');
-      data[n].cat_name = $("input[name=cat-name-text]").val();
-      data[n].click_number = $("input[name=cat-clicks-text]").val();
-      alert ("New content saved!");
-      hide_admin();
-      load_name();
+/*CONTROLLER (interagisce con il MODEL, ovvero i dati*/
+var controller = {
+  loading_names : function (){
+    for(var i=0; i<model.data.length; i++){
+      var temp2;
+      temp2 = temp_name.replace("ID", i).replace("NAME", model.data[i].cat_name);
+      $(".cats_names").append(temp2);
+    }
+  },
+  load_sel_cat : function (i){
+    var temp1;
+    temp1 = temp.replace("ID", i).replace("NAME", model.data[i].cat_name)
+            .replace("IMG", model.data[i].img).replace("X", i).replace("CLICK_ON", model.data[i].click_number);
+    $(".cats").append(temp1);
+    controller.click_counter();
+    $(".btn-admin").show();
+    view.click_admin();
+  },
+  click_counter : function (){
+    $(".cats li").click(function(){
+      var k = $(this).attr('id');
+      model.data[k].click_number ++;
+      $("#count_"+k).html("# clicks: "+model.data[k].click_number);
     })
+  },
+  init_admin_textbox : function (n){
+    $("input[name=cat-name-text]").val(model.data[n].cat_name);
+    $("input[name=cat-clicks-text]").val(model.data[n].click_number);
+  },
+  save : function (){
+    var n=$(".cats li").attr('id');
+    model.data[n].cat_name = $("input[name=cat-name-text]").val();
+    model.data[n].click_number = $("input[name=cat-clicks-text]").val();
+  },
 };
--- data.js
+++ data.js
@@ -1,34 +1,35 @@
-/*list of cat elements*/
-
-var data = [
-  {
-    id: 1,
-    cat_name: "Ubaldo",
-    img: "cat1_image.jpg",
-    click_number: 0
-  },
-  {
-    id: 2,
-    cat_name: "Guido",
-    img: "cat2_image.jpg",
-    click_number: 0
-  },
-  {
-    id: 3,
-    cat_name: "Pippo",
-    img: "cat3_image.jpg",
-    click_number: 0
-  },
-  {
-    id: 4,
-    cat_name: "Ludovico",
-    img: "cat4_image.jpg",
-    click_number: 0
-  },
-  {
-    id: 5,
-    cat_name: "Astolfo",
-    img: "cat5_image.jpg",
-    click_number: 0
-  }
-];
+/*MODEL, l'insieme di tutti i dati*/
+var model = {
+  data : [
+    {
+      id: 1,
+      cat_name: "Ubaldo",
+      img: "cat1_image.jpg",
+      click_number: 0
+    },
+    {
+      id: 2,
+      cat_name: "Guido",
+      img: "cat2_image.jpg",
+      click_number: 0
+    },
+    {
+      id: 3,
+      cat_name: "Pippo",
+      img: "cat3_image.jpg",
+      click_number: 0
+    },
+    {
+      id: 4,
+      cat_name: "Ludovico",
+      img: "cat4_image.jpg",
+      click_number: 0
+    },
+    {
+      id: 5,
+      cat_name: "Astolfo",
+      img: "cat5_image.jpg",
+      click_number: 0
+    }
+  ],
+};
